f9458634dc3956f119b2fb8a496c64adceb15071
almuhandesLLC <144118215+almuhandesLLC@users.noreply.github.com>
2023-11-15 19:36:46 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Type1 from './components/Type1';
 import Type2 from './components/Type2';
 import Tesla from './components/Tesla';
 import Adapters from './components/Adapters';
-import dcAdapters from './components/dcAdapters';
+import dcadapters from './components/dcadapters';
 import ProductPage from './components/ProductPage';
 import Services from './components/Services';
 import Links from './components/Links';
