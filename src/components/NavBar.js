dc7a3c5a648e2cb932d57992a83537c2ab627b70
almuhandesLLC <144118215+almuhandesLLC@users.noreply.github.com>
2023-11-15 20:22:19 +0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -305,7 +305,7 @@ const NAV_ITEMS = [
           {
             label: 'DC Chargers',
             subLabel: 'Find the best DC adapter for your charger',
-               href: '/products/chargers/dc-chargers',
+                  href: '/products/chargers/dc-chargers',
           },
         ],
       },
