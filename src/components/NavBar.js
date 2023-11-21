c512c5490d898fbe85e113863c1b72756f9251e2
almuhandesLLC <144118215+almuhandesLLC@users.noreply.github.com>
2023-11-21 12:41:06 +0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -309,7 +309,7 @@ const NAV_ITEMS = [
           {
             label: 'DC Adapters',
             subLabel: 'Find the best DC adapter for your charger',
-            href: '/products/11',
+            href: '/products/501',
             // href: '/products/11',
             
           },
